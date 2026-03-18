803b27581a74fb7e640c6ae4b9404218af3979dc
Priyanshu <prysh310704@gmail.com>
2026-03-19 00:34:31 +0530

    Changing frontend for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API = import.meta.env.VITE_API_URL;
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${API}/api`,
 });
 
 instance.interceptors.request.use((config) => {
